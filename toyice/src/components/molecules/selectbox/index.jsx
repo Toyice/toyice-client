33085832fdc444a6ad8161b71f9ee18d85196b27
LeeSuBin-khu <subini1707@khu.ac.kr>
2022-08-05 21:17:19 +0900

    [feat & style] 1차 배포 3차 완성(돌멩이)

--- toyice/src/components/molecules/selectbox/index.jsx
+++ toyice/src/components/molecules/selectbox/index.jsx
@@ -5,6 +5,12 @@ import { useRecoilValue } from 'recoil';
 
 import { type } from '../../../pages/project_add';
 
+import Img from '../../atoms/img';
+
+import orange from '../../../assets/svg/stone-orange-eye.svg';
+import yellow from '../../../assets/svg/stone-yellow-eye.svg';
+import blue from '../../../assets/svg/stone-blue-eye.svg';
+
 import './style.css'
 
 const Selectbox = (props) => {
@@ -31,7 +37,7 @@ const Selectbox = (props) => {
     }, [])
 
     return (
-        <div className={!isDrop ? class_props : `selectbox-active ${className}`} ref={selectRef}>
+        <div className={!isDrop ? class_props : `selectbox-active ${className}`} ref={selectRef} style={{position: 'relative'}}>
             <div className={!isDrop ? 'selectbox-placeholder' : 'selectbox-placeholder selectbox-placeholder-hidden' } onClick={selectClickHandler}>
                 <div className={toyType === "아이디어" ? 'orange' :  toyType === "디자인" ? 'yellow' : toyType === "서비스 개발" ? 'blue' : null}>
                     {!isDrop && toyType === "" ? placeholder : toyType}
@@ -45,6 +51,11 @@ const Selectbox = (props) => {
                     <li key={key} id={'selectbox-li-' + key} onClick={onClick}><button key={key} id={'selectbox-btn-' + key}>{btn}</button></li>
                 )}
             </ul>
+            {isDrop && toyType !== "" && <Img className='add-toy-stoneImg' 
+                                    src={toyType === '아이디어' ? orange : toyType === '디자인' ? yellow : toyType === '서비스 개발' ? blue : null}
+                                    width={211.9}
+                                    height={152.71} 
+            />}
         </div>
     );
 };
